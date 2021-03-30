489ee14b87a551c2a9f8a8858a5f525eb1dbc62e
Gautier Arcin <gauta82@gmail.com>
2021-03-30 21:20:42 +0200

    2nd commit

---
+++ src/pages/PilaSuce.tsx
@@ -0,0 +1,8 @@
+const Index = (): JSX.Element => (
+   <div>
+      <h1>Hello world!</h1>
+      <p style={{ color: 'red' }}>Je m'appelle Pila et j'aime Swissex</p>
+   </div>
+);
+
+export default Index;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
-const Index = (): JSX.Element => (
-   <div>
-      <h1>Hello world!</h1>
-      <p>You are using Matthieu LOCUSSOL's starter using Typescript and Next.js!</p>
-   </div>
-);
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const Index = (): JSX.Element => {
+   // const router = useRouter();
+   // console.log(router.query);
+
+   return (
+      <div>
+         <h1>Hello world!</h1>
+         <p>You are using Matthieu LOCUSSOL's starter using Typescript and Next.js!</p>
+         {/* ça bien */}
+         <Link href="/PilaSuce">CLIQUE SALOPE</Link>
+         {/* ça pa bien */}
+         <a href="/PilaSuce">Swissex aussi</a>
+      </div>
+   );
+};
 
 export default Index;
